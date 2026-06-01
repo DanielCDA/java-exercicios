053832021a3bae549254002f5820bb9387789d2b
Daniel CDA <danielagostinho15@gmail.com>
2026-06-01 18:43:19 +0100

    manipular arrey

---
+++ String Manipulação/Estrutura de dados/MemoriaEstatic.java
@@ -0,0 +1,14 @@
+
+public class MemoriaEstatic {
+    public static void main (String[] args){
+        
+        String[] fruta = {"Banana", "Morago", "Abacate"};
+
+        for (int i = 0; i < fruta.length; i++){
+            System.out.println("Numero na posicao " + i + " e a Sua fruta he: " + fruta[i].toUpperCase());
+            System.out.println("A frutas : " + fruta[i]);
+            System.out.println();
+        }
+    }
+    
+}
