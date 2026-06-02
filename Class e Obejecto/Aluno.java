bc961dc280538b3b9d6aebc3fd64ece248d31ed2
Daniel CDA <danielagostinho15@gmail.com>
2026-06-02 19:15:55 +0100

    class e objecto

--- Class e Obejecto/Aluno.java
+++ Class e Obejecto/Aluno.java
@@ -25,13 +25,13 @@ public class Aluno{
 
 
     public static void main (String[] args) {
-        Aluno aluno1 = new Aluno("João", 20);
+        Aluno aluno1 = new Aluno("Joao", 20);
         Aluno aluno2 = new Aluno("Maria", 22);
 
 
         System.out.println("Nome: " + aluno1.getNome());
         System.out.println("Idade: " + aluno1.getIdade());
-
+        System.out.println();
         System.out.println("Nome:" + aluno2.getNome());
         System.out.println("Idade: " + aluno2.getIdade());  
     }
