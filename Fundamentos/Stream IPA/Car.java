c119724ab32241230fbb76161a4e178a181e4534
Daniel CDA <danielagostinho15@gmail.com>
2026-06-19 18:52:01 +0100

    mais testes com stream usando map

--- Fundamentos/Stream IPA/Car.java
+++ Fundamentos/Stream IPA/Car.java
@@ -10,12 +10,24 @@ public class Car {
         carros.add("Toyota");
         carros.add("LExus");
         carros.add("BMw");
-        carros.add("Fors");
+        carros.add("Ford");
 
         System.out.println("== Novos Streams ==");
 
         //usando o forEach
         carros.stream().forEach(carro -> System.out.println(carro));
+
+        //usando o forEach + filter
+        System.out.println("\n == Usando filtro ==");
+        carros.stream()
+        .filter(carro -> carro.startsWith("M"))
+        .forEach(carro -> System.out.println("\n " + carro));
+
+        //usando o forEach + filter + map
+        System.out.println("\n == Usando Map ==");
+        carros.stream()
+        .map(String::toUpperCase)
+        .forEach(carro -> System.err.println(" " + carro));
 
     }
     
